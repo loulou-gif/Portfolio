f038f85631514d206b9a21c1fb4db8e4c38decc8
unknown <konankanjulius10@gmail.com>
2024-02-02 11:27:14 +0000

    cercle

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,11 @@
 import '../Assets/Css/Styles.css'
 const Header = () => {
     return(
-        <header className="">
-            
-            <div className='circle border w-7/12'>
-                <div className='circle1 border bg-white'></div >
-                <div className='circle2 border bg-white'></div >
-                <div className='img-circle border bg-white'></div >
+        <header className="flex-row flex justify-end">
+            <div className='circle border h-2/3 w-7/12'>
+                <div className='circle1  bg-white'></div >
+                <div className='circle2  ml-20 -mt-36 bg-white'></div >
+                <div className='img-circle  ml-28 -mt-52 bg-white'></div >
             </div>
         </header>
     )
