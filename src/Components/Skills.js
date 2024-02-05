12cdd9e9ee3aad88af231e2cf2f66619d701d8d8
unknown <konankanjulius10@gmail.com>
2024-02-05 15:42:30 +0000

    skills start

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,7 +1,58 @@
 import '../Assets/Css/Styles.css'
 const Skills = () => {
     return(
-        <section className="skills"></section>
+        <section className="skills lg:flex lg:justify-center mt-10">
+            <div className='box-skill border lg:flex lg:justify-evenly flex-row w-10/12'>
+                <div className='content-box w-5/12 mt-10 flex p-10 bg-gray-100 border'>
+                    <div className='small-circle border m-5 bg-white'></div>
+                    <div className='skill border text-left w-8/12'>
+                        <div className='box-text text-3xl'>Titre</div>
+                        <ul>
+                            <li className='tex-3xl text-gray-500'>Compétence</li>
+                            <li className='tex-3xl text-gray-500'>Compétence</li>
+                            <li className='tex-3xl text-gray-500'>Compétence</li>
+                            <li className='tex-3xl text-gray-500'>Compétence</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='content-box w-5/12 mt-10 flex p-10 bg-gray-100 border h-2/5'>
+                    <div className='small-circle border m-5 bg-white'></div>
+                    <div className='skill border text-left w-8/12'>
+                        <div className='box-text text-3xl'>Titre</div>
+                        <ul>
+                            <li className='tex-3xl text-gray-500'>Compétence</li>
+                            <li className='tex-3xl text-gray-500'>Compétence</li>
+                            <li className='tex-3xl text-gray-500'>Compétence</li>
+                            <li className='tex-3xl text-gray-500'>Compétence</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='content-box w-5/12 mt-10 flex p-10 bg-gray-100 border'>
+                    <div className='small-circle border m-5 bg-white'></div>
+                    <div className='skill border text-left w-8/12'>
+                        <div className='box-text text-3xl'>Titre</div>
+                        <ul>
+                            <li className='tex-3xl text-gray-500'>Compétence</li>
+                            <li className='tex-3xl text-gray-500'>Compétence</li>
+                            <li className='tex-3xl text-gray-500'>Compétence</li>
+                            <li className='tex-3xl text-gray-500'>Compétence</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='content-box w-5/12 mt-10 flex p-10 bg-gray-100 border h-2/5'>
+                    <div className='small-circle border m-5 bg-white'></div>
+                    <div className='skill border text-left w-8/12'>
+                        <div className='box-text text-3xl'>Titre</div>
+                        <ul>
+                            <li className='tex-3xl text-gray-500'>Compétence</li>
+                            <li className='tex-3xl text-gray-500'>Compétence</li>
+                            <li className='tex-3xl text-gray-500'>Compétence</li>
+                            <li className='tex-3xl text-gray-500'>Compétence</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
---
+++ src/Data/SkillsContent.js
@@ -0,0 +1,40 @@
+const skillsContent = [
+    {
+        name: "Développeur Front-End",
+        icone: "",
+        skill1: "Réact js",
+        skill2: "Tailswind Css",
+        skill3: "Wordpress",
+        skill4: "Booststrap",
+        skill5: "",
+    },
+    {
+        name: "Développeur Back-End",
+        icone: "Django",
+        skill1: "PostgreSql",
+        skill2: "Mysql",
+        skill3: "EC2 AWS",
+        skill4: "Cloud computing", 
+        skill5: "",
+    },
+    {
+        name: "Test Logiciels",
+        icone: "",
+        skill1: "Test Fonctionnel",
+        skill2: "Test de charge",
+        skill3: "Automatisation",
+        skill4: "Rédaction documentation",
+        skill5: "Support",
+    },
+    {
+        name: "Hacking Ethique",
+        icone: "",
+        skill1: "Test d'intrusion",
+        skill2: "Détection failles web",
+        skill3: "Monitoring",
+        skill4: "Formation et conseil client",
+        skill5: "Patch de vulnérabilitées",
+    },
+]
+
+export default skillsContent
